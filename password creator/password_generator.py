b6a8554d8dd6b4400ad05a4e4d7374f4f2d1008f
jenykumari <jenypandey1020@gmail.com>
2026-06-23 13:14:15 +0530

    Update password_generator.py

--- password creator/password_generator.py
+++ password creator/password_generator.py
@@ -1,12 +1,36 @@
 import random
 
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*"
 
 length = int(input("Enter password length: "))
+
+use_numbers = input("Include numbers? (yes/no): ")
+use_symbols = input("Include special characters? (yes/no): ")
+
+characters = letters
+
+if use_numbers.lower() == "yes":
+ characters += numbers
+
+if use_symbols.lower() == "yes":
+ characters += symbols
 
 password = ""
 
 for i in range(length):
-    password = password + random.choice(characters)
+ password += random.choice(characters)
 
+print("\n=== Password Security Report ===")
 print("Generated Password:", password)
+
+if length < 8:
+ print("Strength: Weak")
+
+elif length < 12:
+ print("Strength: Medium")
+
+else:
+ print("Strength: Strong")
+
